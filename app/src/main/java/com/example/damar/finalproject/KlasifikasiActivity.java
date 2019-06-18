68892383ebd19a45e8b81f4cb1854a727aecdb10
dmrgalihrahma <dmrgalihrahma18@gmail.com>
2019-06-19 00:28:34 +0700

    First commit

---
+++ app/src/main/java/com/example/damar/finalproject/KlasifikasiActivity.java
@@ -0,0 +1,97 @@
+package com.example.damar.finalproject;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.damar.finalproject.adapter.ListKlasifikasiAdapter;
+
+import com.example.damar.finalproject.adapter.MyStepperAdapter;
+import com.example.damar.finalproject.database.DatabaseHelper;
+import com.example.damar.finalproject.model.Klasifikasi;
+import com.stepstone.stepper.StepperLayout;
+import com.stepstone.stepper.VerificationError;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+public class KlasifikasiActivity extends AppCompatActivity implements StepperLayout.StepperListener {
+    private RecyclerView lvKlasifikasi;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Klasifikasi> mKlasifikasiList;
+    private DatabaseHelper mDBHelper;
+    private StepperLayout stepperLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_klasifikasi);
+
+        stepperLayout = findViewById(R.id.stepperLayout);
+        stepperLayout.setAdapter(new MyStepperAdapter(getSupportFragmentManager(), this));
+        stepperLayout.setListener(this);
+
+        File database = getApplicationContext().getDatabasePath(DatabaseHelper.DBNAME);
+        if (!database.exists()){
+            mDBHelper.getReadableDatabase();
+            if (copyDatabase(this)){
+                Toast.makeText(this,"COPY SUCCESS",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this,"COPY ERROR",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean copyDatabase(Context context) {
+        try {
+            InputStream inputStream = context.getAssets().open(DatabaseHelper.DBNAME);
+            String outFileName = DatabaseHelper.DBLOCATION + DatabaseHelper.DBNAME;
+            OutputStream outputStream = new FileOutputStream(outFileName);
+            byte[] buff = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buff)) > 0){
+                outputStream.write(buff,0,length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            Log.w("MainActivity","DB copied");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public void onCompleted(View completeButton) {
+        Toast.makeText(this, "onCompleted!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(VerificationError verificationError) {
+        Toast.makeText(this, "onError! -> " + verificationError.getErrorMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStepSelected(int newStepPosition) {
+        Toast.makeText(this, "onStepSelected! -> " + newStepPosition, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onReturn() {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/damar/finalproject/fragment/StepFragmentExample.java
@@ -0,0 +1,104 @@
+package com.example.damar.finalproject.fragment;
+
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.damar.finalproject.R;
+import com.example.damar.finalproject.adapter.MyStepperAdapter;
+import com.example.damar.finalproject.database.DatabaseHelper;
+import com.example.damar.finalproject.model.Klasifikasi;
+import com.stepstone.stepper.Step;
+import com.stepstone.stepper.VerificationError;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StepFragmentExample extends Fragment implements Step {
+
+
+    public StepFragmentExample() {
+        // Required empty public constructor
+    }
+
+    RadioGroup mRadioGroup;
+    RadioButton mRadioButton1;
+    RadioButton mRadioButton2;
+    RadioButton mRadioButton3;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Bundle bundle = this.getArguments();
+        Log.d("##", "onCreateView: " + bundle.getInt(MyStepperAdapter.CURRENT_STEP_POSITION_KEY)) ;
+
+        View view = inflater.inflate(R.layout.fragment_step_fragment_example, container, false);
+
+        mRadioGroup = view.findViewById(R.id.radioGroupWrapper);
+
+        DatabaseHelper mDBHelper = new DatabaseHelper(getContext());
+        Log.d("#######", String.valueOf(bundle.getInt(MyStepperAdapter.CURRENT_STEP_POSITION_KEY)));
+        final List<Klasifikasi> lk = mDBHelper.getListKlasifikasiByGroup(String.valueOf(Integer.valueOf(bundle.getInt(MyStepperAdapter.CURRENT_STEP_POSITION_KEY)) + 1));
+        final RadioButton[] rb = new RadioButton[lk.size()];
+        Log.d("##sz", "onCreateView: " + lk.size()) ;
+
+
+        int[][] states = new int[][] {
+                new int[] { android.R.attr.state_enabled}, // enabled
+                new int[] {-android.R.attr.state_enabled}, // disabled
+                new int[] {-android.R.attr.state_checked}, // unchecked
+                new int[] { android.R.attr.state_pressed}  // pressed
+        };
+
+        int[] colors = new int[] {
+                Color.BLACK,
+                Color.BLACK,
+                Color.BLACK,
+                Color.BLACK
+        };
+
+        mRadioButton1 = view.findViewById(R.id.checkbox1);
+        mRadioButton2 = view.findViewById(R.id.checkbox2);
+        mRadioButton3 = view.findViewById(R.id.checkbox3);
+
+        if(lk.size() > 0) {
+            Klasifikasi kl0 = lk.get(0);
+            Klasifikasi kl1 = lk.get(1);
+            Klasifikasi kl2 = lk.get(2);
+            mRadioButton1.setText(kl0.getKlasifikasi());
+            mRadioButton2.setText(kl1.getKlasifikasi());
+            mRadioButton3.setText(kl2.getKlasifikasi());
+        }
+
+        return view;
+    }
+
+    @Nullable
+    @Override
+    public VerificationError verifyStep() {
+        return null;
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onError(@NonNull VerificationError error) {
+
+    }
+}
