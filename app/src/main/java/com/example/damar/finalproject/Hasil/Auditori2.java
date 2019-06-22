a018c1b63467d6a51444df3f5dee1f98203fb383
dmrgalihrahma <dmrgalihrahma18@gmail.com>
2019-06-22 16:46:49 +0700

    Fitur Konsultasi Metode 2

---
+++ app/src/main/java/com/example/damar/finalproject/Hasil/Auditori2.java
@@ -0,0 +1,23 @@
+package com.example.damar.finalproject.Hasil;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.damar.finalproject.R;
+
+public class Auditori2 extends AppCompatActivity {
+    public TextView metode1, metode2, keterangan1;
+    String hasil1 = "ceramah";
+    String ket1 = "bla bla bla";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+        metode1 = (TextView) findViewById(R.id.txt_metode1);
+        keterangan1 = (TextView) findViewById(R.id.txt_ket1);
+        metode1.setText(hasil1);
+        keterangan1.setText(ket1);
+    }
+}
